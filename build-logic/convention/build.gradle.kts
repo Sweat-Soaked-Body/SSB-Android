19b1d1da0148a51e932ab8f161338ee7a6b59163
명훈 <s23065@gsm.hs.kr>
2024-09-11 21:14:05 +0900

    :memo: :: Register Plugins(:build-logic:convention)

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -20,3 +20,42 @@ dependencies {
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "sweat.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+
+        register("androidHilt") {
+            id = "sweat.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+
+        register("androidLint") {
+            id = "sweat.android.lint"
+            implementationClass = "AndroidLintConventionPlugin"
+        }
+
+        register("androidCore") {
+            id = "sweat.android.core"
+            implementationClass = "AndroidCoreConventionPlugin"
+        }
+
+        register("androidCompose") {
+            id = "sweat.android.compose"
+            implementationClass = "AndroidComposeConventionPlugin"
+        }
+
+        register("jvmLibrary") {
+            id = "sweat.jvm.library"
+            implementationClass = "JvmLibraryConventionPlugin"
+        }
+
+        register("androidFeature") {
+            id = "sweat.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+    }
+}
