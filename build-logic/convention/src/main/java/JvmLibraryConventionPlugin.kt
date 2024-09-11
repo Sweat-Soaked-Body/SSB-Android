9121001b187d0781e785c37e726bc4f19c928e63
명훈 <s23065@gsm.hs.kr>
2024-09-11 21:19:48 +0900

    :memo: :: Update JvmLibraryConventionPlugin(Use Custom Plugin)

--- build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
@@ -11,7 +11,7 @@ class JvmLibraryConventionPlugin : Plugin<Project> {
             // Apply necessary plugins for JVM libraries
             with(pluginManager) {
                 apply("org.jetbrains.kotlin.jvm")  // Apply Kotlin JVM plugin for Kotlin support in JVM projects
-                apply("build.android.lint")  // Apply Android Lint plugin for code quality checks (custom plugin)
+                apply("sweat.android.lint")  // Apply Android Lint plugin for code quality checks (custom plugin)
                 apply("com.google.devtools.ksp")  // Apply KSP (Kotlin Symbol Processing) plugin for code generation
             }
 
