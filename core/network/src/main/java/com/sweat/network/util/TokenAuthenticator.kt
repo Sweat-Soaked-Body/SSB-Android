2d299c8047a29012753071ab56b61f1ababc2a9c
명훈 <s23065@gsm.hs.kr>
2024-09-13 09:38:06 +0900

    :sparkles: :: Add TokenAuthenticator(Implementation Incomplete)

---
+++ core/network/src/main/java/com/sweat/network/util/TokenAuthenticator.kt
@@ -0,0 +1,15 @@
+package com.sweat.network.util
+
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import javax.inject.Inject
+
+class TokenAuthenticator @Inject constructor(
+    // todo : Add LocalDataSource
+) : Authenticator {
+    override fun authenticate(route: Route?, response: Response): Request? {
+        TODO("Not yet implemented")
+    }
+}
