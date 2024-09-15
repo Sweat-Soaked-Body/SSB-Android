bc6339c08bb017ff0c2f4df8e4003e7ff0fe566a
명훈 <s23065@gsm.hs.kr>
2024-09-15 22:43:02 +0900

    :sparkles: :: Create DataStoreModule / Add Comment

---
+++ core/datastore/src/main/java/com/sweat/datastore/di/DataStoreModule.kt
@@ -0,0 +1,18 @@
+package com.sweat.datastore.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthTokenDataStore() {
+        // todo : Add DataStore Instance
+    }
+}
