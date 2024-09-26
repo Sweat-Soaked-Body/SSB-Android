5bce522cc4414c928f8c4ebcc588454258114988
명훈 <s23065@gsm.hs.kr>
2024-09-26 20:24:28 +0900

    :memo: :: Apply DevicePreviews

--- app/src/main/java/com/sweat/ssb_android/MainActivity.kt
+++ app/src/main/java/com/sweat/ssb_android/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.sweat.ssb_android.ui.theme.SSBAndroidTheme
+import com.sweat.ui.DevicePreviews
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,13 +38,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Box(
         modifier = modifier
-    )
+            .background(color = Color.White)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@DevicePreviews
 @Composable
 fun GreetingPreview() {
     SSBAndroidTheme {
