b811d64e5baa5e15f0fd04aeff005e41cea468a4
명훈 <s23065@gsm.hs.kr>
2024-10-23 12:25:36 +0900

    :memo: :: Apply Before Commit(Create AppState, App, TopLevelDestination)

--- app/src/main/java/com/sweat/ssb_android/MainActivity.kt
+++ app/src/main/java/com/sweat/ssb_android/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.CompositionLocalProvider
+import com.sweat.ssb_android.ui.SSBApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {  }
+        setContent { 
+            CompositionLocalProvider {
+                SSBApp(windowSizeClass = calculateWindowSizeClass(this))
+            }
+        }
     }
 }
