c9aceb784a2eb9c0718c574166bd7738b4839b27
명훈 <s23065@gsm.hs.kr>
2024-10-23 11:28:30 +0900

    :fire: :: Unused MainActivity Code

--- app/src/main/java/com/sweat/ssb_android/MainActivity.kt
+++ app/src/main/java/com/sweat/ssb_android/MainActivity.kt
@@ -4,17 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.sweat.ssb_android.ui.theme.SSBAndroidTheme
-import com.sweat.ui.DevicePreviews
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,37 +11,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            SSBAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
-            .background(color = Color.White)
-            .fillMaxSize()
-    ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-}
-
-@DevicePreviews
-@Composable
-fun GreetingPreview() {
-    SSBAndroidTheme {
-        Greeting("Android")
+        setContent {  }
     }
 }
