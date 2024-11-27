fd139bcbb532613ebbb72d4fc38108f3731b15ed
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2024-11-27 20:21:46 +0900

    add LoginScreen

---
+++ feature/login/src/main/java/com/sweat/login/LoginScreen.kt
@@ -0,0 +1,124 @@
+package com.sweat.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sweat.design_system.component.button.ButtonState
+import com.sweat.design_system.component.button.SSBButton
+import com.sweat.design_system.component.modifier.clickableSingle
+import com.sweat.design_system.component.textfield.SSBTextField
+import com.sweat.design_system.icon.EyeIcon
+import com.sweat.design_system.theme.SSBTypography
+import com.sweat.design_system.theme.color.SSBColor
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier
+) {
+    val (idTextState, onIdTextChange) = remember { mutableStateOf("") }
+    val (passwordTextState, onPasswordTextChange) = remember { mutableStateOf("") }
+    val (isSelected, setIsSelected) = remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(horizontal = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = modifier.height(55.dp))
+
+        Text(
+            text = "Sweet-soaked-body",
+            style = SSBTypography.label.copy(
+                fontFamily = FontFamily(Font(com.sweat.design_system.R.font.rookie))
+            ),
+            color = SSBColor.main
+        )
+        Text(
+            text = "함께 땀 흘리며 열정을 나눌 동료를 찾습니다",
+            style = SSBTypography.bodySmall,
+            color = SSBColor.gray600
+        )
+
+        Spacer(modifier = modifier.height(34.dp))
+
+        Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+            SSBTextField(
+                textState = idTextState,
+                label = "아이디",
+                placeHolder = "아이디를 입력해주세요",
+                onTextChange = onIdTextChange,
+                icon = {}
+            )
+
+            SSBTextField(
+                textState = passwordTextState,
+                label = "비밀번호",
+                placeHolder = "비밀번호를 입력해주세요",
+                onTextChange = onPasswordTextChange,
+                icon = {
+                    EyeIcon(
+                        isSelected = isSelected,
+                        modifier = Modifier.clickableSingle { setIsSelected(!isSelected) }
+                    )
+                },
+                visualTransformation = if (isSelected) VisualTransformation.None else PasswordVisualTransformation()
+            )
+        }
+
+        Spacer(modifier = modifier.weight(1f))
+
+        SSBButton(
+            modifier = modifier.fillMaxWidth(),
+            text = "다음",
+            state = if (idTextState.isEmpty() || passwordTextState.isEmpty()) ButtonState.Enabled else ButtonState.Disabled,
+            onClick = { /*TODO*/ }
+        )
+
+        Spacer(modifier = modifier.height(6.dp))
+
+        Text(
+            text = "아직 계정이 없으신가요?",
+            style = SSBTypography.regularCaption,
+            color = SSBColor.gray300
+        )
+
+        Text(
+            modifier = modifier.clickableSingle { /*TODO*/ },
+            text = "회원가입",
+            style = SSBTypography.regularTextSM,
+            color = SSBColor.main
+        )
+        
+        Spacer(modifier = modifier.height(38.dp))
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
