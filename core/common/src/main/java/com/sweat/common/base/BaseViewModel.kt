78bfc1a1105507e22dcdb0cf62f129d7630e7878
youloveli <mymhj122@gmail.com>
2024-12-03 16:26:51 +0900

    postSideEffect의 코루틴 스코프의 런치를 io스래드가 아닌 main 스레드에서 하도록 변경

--- core/common/src/main/java/com/sweat/common/base/BaseViewModel.kt
+++ core/common/src/main/java/com/sweat/common/base/BaseViewModel.kt
@@ -7,10 +7,10 @@ import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 /**
-* @param S : State
-* @param E : SideEffect
-* @param I : Intent
-*/
+ * @param S : State
+ * @param E : SideEffect
+ * @param I : Intent
+ */
 abstract class BaseViewModel<S, E, I>(initialState: S) : ViewModel() {
     private val _state: MutableStateFlow<S> = MutableStateFlow(initialState)
     val state = _state.asStateFlow()
@@ -23,7 +23,7 @@ abstract class BaseViewModel<S, E, I>(initialState: S) : ViewModel() {
     }
 
     protected fun postSideEffect(sideEffect: E) {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(Dispatchers.Main) {
             _sideEffect.emit(sideEffect)
         }
     }
