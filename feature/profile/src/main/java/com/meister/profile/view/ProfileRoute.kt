1ecd62bfec00c9394685aaa8f3d4a3fde3e78de0
youloveli <mymhj122@gmail.com>
2024-12-03 17:58:53 +0900

    ProfileRoute에 viewModel 연결

--- feature/profile/src/main/java/com/meister/profile/view/ProfileRoute.kt
+++ feature/profile/src/main/java/com/meister/profile/view/ProfileRoute.kt
@@ -1,7 +1,51 @@
 package com.meister.profile.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.meister.profile.viewModel.ProfileIntent
+import com.meister.profile.viewModel.ProfileScreenSideEffect
+import com.meister.profile.viewModel.ProfileScreenState
+import com.meister.profile.viewModel.ProfileViewModel
+import kotlin.reflect.KFunction1
 
 @Composable
-fun ProfileRoute() {
+fun ProfileRoute(
+    modifier: Modifier = Modifier,
+    viewModel: ProfileViewModel = hiltViewModel(),
+) {
+    LaunchedEffect(Unit) {
+        viewModel.sideEffect.collect { sideEffect ->
+            when (sideEffect) {
+                ProfileScreenSideEffect.LaunchAddFriendBottomSheet -> TODO()
+                is ProfileScreenSideEffect.LaunchImagePicker -> TODO()
+                ProfileScreenSideEffect.LaunchSettingBottomSheet -> TODO()
+                ProfileScreenSideEffect.NavigateToAddFriendWithNFC -> TODO()
+                ProfileScreenSideEffect.NavigateToAddFriendWithQR -> TODO()
+                is ProfileScreenSideEffect.NavigateToChat -> TODO()
+                ProfileScreenSideEffect.NavigateToLogin -> TODO()
+                ProfileScreenSideEffect.NavigateToMyQR -> TODO()
+                ProfileScreenSideEffect.ShowSecessionPopup -> TODO()
+            }
+        }
+    }
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    ProfileScreen(
+        modifier = modifier,
+        state = state,
+        handleIntent = viewModel::handleIntent
+    )
+}
+
+@Composable
+fun ProfileScreen(
+    modifier: Modifier = Modifier,
+    state: ProfileScreenState,
+    handleIntent: KFunction1<ProfileIntent, Unit>
+) {
+
 }
