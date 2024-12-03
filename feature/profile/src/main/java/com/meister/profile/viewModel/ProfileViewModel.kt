911721b15177594c5c671a1fad25a15017a59fc1
youloveli <mymhj122@gmail.com>
2024-12-03 17:48:31 +0900

    viewModel 구현

---
+++ feature/profile/src/main/java/com/meister/profile/viewModel/ProfileViewModel.kt
@@ -0,0 +1,102 @@
+package com.meister.profile.viewModel
+
+import com.sweat.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+
+) : BaseViewModel<ProfileScreenState, ProfileScreenSideEffect, ProfileIntent>(ProfileScreenState.getInitialState()) {
+    override fun handleIntent(intent: ProfileIntent) {
+        when (intent) {
+            ProfileIntent.AddFriend -> postSideEffect(ProfileScreenSideEffect.LaunchAddFriendBottomSheet)
+            ProfileIntent.AddFriendWithNFC -> postSideEffect(ProfileScreenSideEffect.NavigateToAddFriendWithNFC)
+            ProfileIntent.AddFriendWithQR -> postSideEffect(ProfileScreenSideEffect.NavigateToAddFriendWithQR)
+            ProfileIntent.AddProfilePicture -> postSideEffect(ProfileScreenSideEffect.LaunchImagePicker(200))
+            ProfileIntent.StartEditProfile -> { setState { state.value.copy(isProfileEditing = true) } }
+            ProfileIntent.EndEditProfile -> postProfileEdit()
+            ProfileIntent.Logout -> logout()
+            ProfileIntent.Secession -> postSideEffect(ProfileScreenSideEffect.ShowSecessionPopup)
+            ProfileIntent.Setting -> postSideEffect(ProfileScreenSideEffect.LaunchSettingBottomSheet)
+            ProfileIntent.ShowMyQR -> postSideEffect(ProfileScreenSideEffect.NavigateToMyQR)
+            is ProfileIntent.StartChat -> postSideEffect(ProfileScreenSideEffect.NavigateToChat(id = intent.id))
+            is ProfileIntent.SetMyIntro -> setState { state.value.copy(myIntro = intent.state) }
+            is ProfileIntent.SetProfileImage -> setState { state.value.copy(image = intent.image) }
+        }
+    }
+
+    private fun loadChatList() {
+
+    }
+
+    private fun loadProfileData() {
+
+    }
+
+    private fun postProfileEdit() {
+        // TODO: 통신이 성공하면 실행시키도록 변경
+        setState { state.value.copy(isProfileEditing = false) }
+    }
+
+    private fun logout() {
+        // TODO: 토큰들 지우기
+        postSideEffect(ProfileScreenSideEffect.NavigateToLogin)
+    }
+}
+
+data class ProfileScreenState(
+    val myName: String,
+    val myIntro: String,
+    val image: String,
+    val isProfileEditing: Boolean,
+    val chatList: ImmutableList<ChatListItemState>,
+) {
+    companion object {
+        // State의 초기값을 넣어주기위해 필수로 구현해야하는 함수
+        fun getInitialState() = ProfileScreenState(
+            myName = "",
+            myIntro = "",
+            image = "",
+            isProfileEditing = false,
+            chatList = persistentListOf(),
+        )
+    }
+}
+
+data class ChatListItemState(
+    val name: String,
+    val message: String,
+    val date: String,
+)
+
+sealed class ProfileScreenSideEffect {
+    object ShowSecessionPopup : ProfileScreenSideEffect()
+    data class LaunchImagePicker(val requestCode: Int) : ProfileScreenSideEffect()
+    object LaunchAddFriendBottomSheet : ProfileScreenSideEffect()
+    object LaunchSettingBottomSheet : ProfileScreenSideEffect()
+    object NavigateToLogin : ProfileScreenSideEffect()
+    object NavigateToMyQR : ProfileScreenSideEffect()
+    object NavigateToAddFriendWithQR : ProfileScreenSideEffect()
+    object NavigateToAddFriendWithNFC : ProfileScreenSideEffect()
+    data class NavigateToChat(val id: String) : ProfileScreenSideEffect()
+}
+
+
+sealed class ProfileIntent {
+    object Setting : ProfileIntent()
+    data class StartChat(val id: String) : ProfileIntent()
+    data class SetMyIntro(val state: String) : ProfileIntent()
+    data class SetProfileImage(val image: String) : ProfileIntent()
+    object StartEditProfile : ProfileIntent()
+    object EndEditProfile : ProfileIntent()
+    object Logout : ProfileIntent()
+    object Secession : ProfileIntent()
+    object ShowMyQR : ProfileIntent()
+    object AddProfilePicture : ProfileIntent()
+    object AddFriend : ProfileIntent()
+    object AddFriendWithQR : ProfileIntent()
+    object AddFriendWithNFC : ProfileIntent()
+}
