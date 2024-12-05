3f8b4454da1e977e5b7a0547fbf19cadd37fd2f4
youloveli <mymhj122@gmail.com>
2024-12-05 15:19:29 +0900

    box 추가

--- feature/profile/src/main/java/com/meister/profile/view/ProfileRoute.kt
+++ feature/profile/src/main/java/com/meister/profile/view/ProfileRoute.kt
@@ -3,6 +3,7 @@ package com.meister.profile.view
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,12 +15,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,6 +40,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.meister.profile.viewModel.BottomSheetType
 import com.meister.profile.viewModel.ChatListItemState
 import com.meister.profile.viewModel.ProfileIntent
 import com.meister.profile.viewModel.ProfileScreenSideEffect
@@ -43,10 +50,14 @@ import com.sweat.common.utill.decodeBase64Image
 import com.sweat.design_system.component.modifier.clickableSingle
 import com.sweat.design_system.icon.AddFriendIcon
 import com.sweat.design_system.icon.CheckIcon
+import com.sweat.design_system.icon.OutIcon
+import com.sweat.design_system.icon.PencilIcon
 import com.sweat.design_system.icon.SettingIcon
+import com.sweat.design_system.icon.TrashIcon
 import com.sweat.design_system.theme.SSBTypography
 import com.sweat.design_system.theme.color.SSBColor
 import com.sweat.ui.DevicePreviews
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -145,63 +156,65 @@ fun ProfileScreen(
             )
         }
     ) { paddingValues ->
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(horizontal = 24.dp),
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(horizontal = 24.dp),
             ) {
-                Image(
-                    painter = getProfileImage(state.image),
-                    contentDescription = "profileImage",
-                    modifier = Modifier
-                        .padding(15.dp)
-                        .size(60.dp),
-                )
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp),
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
                 ) {
-                    Text(
-                        text = state.myName,
-                        style = SSBTypography.subTitle,
-                        fontWeight = FontWeight(600),
-                        color = Color(0xFF000000),
+                    Image(
+                        painter = getProfileImage(state.image),
+                        contentDescription = "profileImage",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(60.dp),
                     )
-                    Spacer(modifier = Modifier.height(7.dp))
-                    BasicTextField(
-                        value = state.myIntro,
-                        onValueChange = {
-                            handleIntent(ProfileIntent.SetMyIntro(it))
-                        },
-                        textStyle = MaterialTheme.typography.bodyLarge.copy(color = Color.Black),
-                        cursorBrush = SolidColor(Color.Black),
-                        decorationBox = { innerTextField ->
-                            if (state.myIntro.isEmpty()) {
-                                Text("한 줄 소개를 적어주세요", color = Color.Gray)
-                            }
-                            innerTextField()
-                        },
+                    Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 8.dp)
-                            .background(Color.Transparent, shape = RectangleShape)
-                    )
-                    Text(
-                        text = state.myIntro,
-                        style = SSBTypography.bodySmall,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF000000),
-                    )
+                            .padding(15.dp),
+                    ) {
+                        Text(
+                            text = state.myName,
+                            style = SSBTypography.subTitle,
+                            fontWeight = FontWeight(600),
+                            color = Color(0xFF000000),
+                        )
+                        Spacer(modifier = Modifier.height(7.dp))
+                        BasicTextField(
+                            value = state.myIntro,
+                            onValueChange = {
+                                handleIntent(ProfileIntent.SetMyIntro(it))
+                            },
+                            textStyle = MaterialTheme.typography.bodyLarge.copy(color = Color.Black),
+                            cursorBrush = SolidColor(Color.Black),
+                            decorationBox = { innerTextField ->
+                                if (state.myIntro.isEmpty()) {
+                                    Text("한 줄 소개를 적어주세요", color = Color.Gray)
+                                }
+                                innerTextField()
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp)
+                                .background(Color.Transparent, shape = RectangleShape)
+                        )
+                        Text(
+                            text = state.myIntro,
+                            style = SSBTypography.bodySmall,
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF000000),
+                        )
+                    }
                 }
-            }
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(state.chatList) { state ->
-                    ChatListItem(state = state)
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(state.chatList) { state ->
+                        ChatListItem(state = state)
+                    }
                 }
             }
 
