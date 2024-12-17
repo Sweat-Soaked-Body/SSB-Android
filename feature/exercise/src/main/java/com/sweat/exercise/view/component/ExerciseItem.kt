c567990756db5b3bb93b2043fea329c9d7469241
손찬형 <sonchanhyeong710@gmail.com>
2024-12-17 18:20:31 +0900

    ExerciseItem 생성

---
+++ feature/exercise/src/main/java/com/sweat/exercise/view/component/ExerciseItem.kt
@@ -0,0 +1,66 @@
+package com.sweat.exercise.view.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sweat.design_system.icon.HeartIcon
+import com.sweat.design_system.theme.SSBTypography
+
+@Composable
+fun ExerciseItem(
+    modifier: Modifier,
+    text: String,
+    isSelected: Boolean = false,
+    onHeartClick: () -> Unit
+){
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(
+                vertical = 17.dp,
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            style = SSBTypography.bodySmall
+        )
+        Spacer(modifier = modifier.weight(1f))
+        HeartIcon(
+            modifier = modifier
+                .padding(1.dp)
+                .width(24.dp)
+                .height(24.dp)
+                .clickable{ onHeartClick() },
+            isSelected = isSelected
+        )
+    }
+    Divider(thickness = 1.dp, color = Color(0xFFEFF0F2))
+}
+
+@Preview
+@Composable
+fun ExerciseItemPreview(){
+    ExerciseItem(
+        modifier = Modifier,
+        text = "바벨 백스쿼트",
+        isSelected = true,
+        onHeartClick = {}
+    )
+}
