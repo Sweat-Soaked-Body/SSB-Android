1c2146c95ce457d3c272b32089881cdaa64805a8
youloveli <mymhj122@gmail.com>
2024-12-18 17:42:57 +0900

    feature: friend 엔드포인트의 api들의 repository interFace 추가

---
+++ core/data/src/main/java/com/sweat/data/repository/friend/FriendRepository.kt
@@ -0,0 +1,9 @@
+package com.sweat.data.repository.friend
+
+import kotlinx.coroutines.flow.Flow
+
+interface FriendRepository {
+    fun friendCheck(id: Int, fromUser: Int, toUser: Int): Flow<List<String>>
+    fun friendAdd(toUser: Int): Flow<Unit>
+    fun friendDelete(id: String): Flow<Unit>
+}
