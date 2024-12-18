954d0650185f1a7eaa73a1b227ad5538204c21ce
명훈 <s23065@gsm.hs.kr>
2024-12-18 23:22:06 +0900

    :recycle: :: Add keyboardOptions

--- feature/signup/src/main/java/com/sweat/signup/component/SignupTextField.kt
+++ feature/signup/src/main/java/com/sweat/signup/component/SignupTextField.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,6 +29,7 @@ fun SignupTextField(
     helperText: String = "",
     isError: Boolean = false,
     onTextChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
 ) {
     Column(
         modifier = modifier.fillMaxWidth(),
@@ -52,7 +55,8 @@ fun SignupTextField(
                     }
                     innerTextField()
                 }
-            }
+            },
+            keyboardOptions = keyboardOptions
         )
         Text(
             text = helperText,
