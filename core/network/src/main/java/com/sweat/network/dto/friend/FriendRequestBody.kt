518c556288cd23093764ce29e4c961ecc41c3170
youloveli <mymhj122@gmail.com>
2024-12-18 16:07:56 +0900

    feature: 친구조회api 에서 body로 쓰이는 FriendRequestBody dataclass 추가

---
+++ core/network/src/main/java/com/sweat/network/dto/friend/FriendRequestBody.kt
@@ -0,0 +1,11 @@
+package com.sweat.network.dto.friend
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class FriendRequestBody(
+    @Json(name = "id") val id: Int,
+    @Json(name = "from_user") val fromUser: Int,
+    @Json(name = "to_user") val toUser: Int
+)
