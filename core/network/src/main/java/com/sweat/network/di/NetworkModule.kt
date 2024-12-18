b6c7d4312398e0586c27ec6f43dec71f7a51610f
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2024-12-18 10:10:33 +0900

    add login NetworkModule

--- core/network/src/main/java/com/sweat/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/sweat/network/di/NetworkModule.kt
@@ -7,6 +7,7 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.sweat.network.api.AuthApi
 import com.sweat.network.util.AuthInterceptor
+import com.sweat.network.util.SimpleCookieJar
 import com.sweat.network.util.TokenAuthenticator
 import dagger.Module
 import dagger.Provides
@@ -35,10 +36,11 @@ object NetworkModule {
         @ApplicationContext context: Context,
         httpLoggingInterceptor: HttpLoggingInterceptor,
         authInterceptor: AuthInterceptor,
-        tokenAuthenticator: TokenAuthenticator
+        tokenAuthenticator: TokenAuthenticator,
+        cookieJar: CookieJar
     ): OkHttpClient {
         return OkHttpClient.Builder()
-            .cookieJar(CookieJar.NO_COOKIES)
+            .cookieJar(cookieJar)
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
             .writeTimeout(30, TimeUnit.SECONDS)
@@ -52,6 +54,12 @@ object NetworkModule {
                     .retainDataFor(ChuckInterceptor.Period.ONE_HOUR)
             )
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCookieJar(): CookieJar {
+        return SimpleCookieJar() // 쿠키 저장소
     }
 
     @Provides
@@ -73,7 +81,7 @@ object NetworkModule {
         moshiConverterFactory: MoshiConverterFactory
     ): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("") // todo : Add BaseUrl - Use BuildConfig
+            .baseUrl("https://api.gsm-ssb.shop") // todo : Add BaseUrl - Use BuildConfig
             .client(okHttpClient)
             .addConverterFactory(moshiConverterFactory)
             .build()
