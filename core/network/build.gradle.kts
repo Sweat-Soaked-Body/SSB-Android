629ebb7a5894ec399e3ba2a729c9a75bea8db1aa
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2024-12-18 10:06:25 +0900

    gradle 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:model"))
 
+
     debugImplementation(libs.debug.chuck)
     releaseImplementation(libs.release.chuck)
 
