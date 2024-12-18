54fe928134f10a139ae855d773d5a0f9b33fcde3
youloveli <mymhj122@gmail.com>
2024-12-18 16:50:18 +0900

    feature: 잘못된 api 스팩 변경

--- core/network/src/main/java/com/sweat/network/api/FriendApi.kt
+++ core/network/src/main/java/com/sweat/network/api/FriendApi.kt
@@ -1,26 +1,28 @@
 package com.sweat.network.api
 
-import com.sweat.network.dto.auth.request.LoginRequest
-import com.sweat.network.dto.friend.FriendRequestBody
+import com.sweat.network.dto.friend.AddFriendRequest
+import com.sweat.network.dto.friend.FriendCheckRequest
+import com.sweat.network.dto.friend.FriendResponse
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface FriendApi {
     @GET("/friend")
     suspend fun friendCheck(
-        @Body body: FriendRequestBody
-    ): List<String>
+        @Body body: FriendCheckRequest,
+    ): FriendResponse
 
     @POST("/friend")
     suspend fun friendAdd(
-        @Body body: LoginRequest
+        @Body body: AddFriendRequest,
     )
 
-    @DELETE("/friend")
+    @DELETE("/friend/{id}")
     suspend fun friendDelete(
-        @Query("id") id: String,
+        @Path("id") id: String,
     )
 }
