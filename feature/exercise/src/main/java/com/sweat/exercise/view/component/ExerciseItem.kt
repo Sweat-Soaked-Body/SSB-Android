acd87725650bf03b81a4e0a1c86637da8527e44b
손찬형 <sonchanhyeong710@gmail.com>
2024-12-19 16:49:53 +0900

    remove unused import

--- feature/exercise/src/main/java/com/sweat/exercise/view/component/ExerciseItem.kt
+++ feature/exercise/src/main/java/com/sweat/exercise/view/component/ExerciseItem.kt
@@ -1,15 +1,12 @@
 package com.sweat.exercise.view.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
