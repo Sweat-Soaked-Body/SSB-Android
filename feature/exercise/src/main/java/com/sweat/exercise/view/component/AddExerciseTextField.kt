8d75b18e46e617fb6118d3fa0704e670c458dbe7
손찬형 <sonchanhyeong710@gmail.com>
2024-12-19 17:14:19 +0900

    rename ExerciseSelector & ExerciseTextField

--- feature/exercise/src/main/java/com/sweat/exercise/view/component/AddExerciseTextField.kt
+++ feature/exercise/src/main/java/com/sweat/exercise/view/component/AddExerciseTextField.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
