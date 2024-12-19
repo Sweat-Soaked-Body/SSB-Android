4a87a3c6ca3be287722697781d9f402c83b9ed80
명훈 <s23065@gsm.hs.kr>
2024-12-19 20:52:02 +0900

    :memo: :: Add Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(project(":feature:signup"))
     implementation(project(":feature:profile"))
     implementation(project(":feature:login"))
+    implementation(project(":core:common"))
 
     implementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext)
