b252d4309c9e9bf3af33c6353cabfed267d71309
명훈 <s23065@gsm.hs.kr>
2024-12-19 20:50:34 +0900

    :recycle: :: Refactor AuthRepository

--- core/data/src/main/java/com/sweat/data/repository/auth/AuthRepository.kt
+++ core/data/src/main/java/com/sweat/data/repository/auth/AuthRepository.kt
@@ -1,8 +1,10 @@
 package com.sweat.data.repository.auth
 
 import com.sweat.model.param.auth.LoginRequestParam
+import com.sweat.model.param.auth.SignUpRequestParam
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
     fun login(body: LoginRequestParam): Flow<Unit>
+    fun signUp(body: SignUpRequestParam) : Flow<Unit>
 }
