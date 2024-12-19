474331ea7c696701d0229b23a0c3dde7c2c7bfa6
명훈 <s23065@gsm.hs.kr>
2024-12-19 20:53:28 +0900

    :sparkles: :: Add SignUp API

--- core/network/src/main/java/com/sweat/network/api/AuthApi.kt
+++ core/network/src/main/java/com/sweat/network/api/AuthApi.kt
@@ -1,6 +1,7 @@
 package com.sweat.network.api
 
 import com.sweat.network.dto.auth.request.LoginRequest
+import com.sweat.network.dto.auth.request.SignUpRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -8,5 +9,10 @@ interface AuthApi {
     @POST("/auth/signin")
     suspend fun login(
         @Body body: LoginRequest
+    )
+
+    @POST("/auth/signup")
+    suspend fun signUp(
+        @Body body: SignUpRequest
     )
 }
