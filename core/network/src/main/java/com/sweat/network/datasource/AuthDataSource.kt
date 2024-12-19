1a9711fb256ad7aac3a4164d9628c54ce85b527f
명훈 <s23065@gsm.hs.kr>
2024-12-19 20:42:49 +0900

    :fire: :: Fire Unused FIles

--- core/network/src/main/java/com/sweat/network/datasource/AuthDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.sweat.network.datasource
-
-import com.sweat.network.dto.auth.request.LoginRequest
-import kotlinx.coroutines.flow.Flow
-
-interface AuthDataSource {
-    fun authLogin(body: LoginRequest): Flow<Unit>
-}
