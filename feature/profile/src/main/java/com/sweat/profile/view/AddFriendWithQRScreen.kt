f00348acf47c32c2ecf7af08fc2054ebcd58451c
youloveli <mymhj122@gmail.com>
2024-12-20 09:53:12 +0900

    feat: route에서 권한 상태, 친구추가 성공 여부애 따라 다른 화면을 띄우도록 변경

--- feature/profile/src/main/java/com/sweat/profile/view/AddFriendWithQRScreen.kt
+++ feature/profile/src/main/java/com/sweat/profile/view/AddFriendWithQRScreen.kt
@@ -4,12 +4,9 @@ import android.Manifest
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -33,13 +30,13 @@ import com.google.accompanist.permissions.shouldShowRationale
 import com.sweat.design_system.component.modifier.clickableSingle
 import com.sweat.design_system.icon.ChevronLeftIcon
 import com.sweat.profile.R
-import com.sweat.profile.component.AddFriendSuccessCard
+import com.sweat.profile.component.AddFriendSuccessScreen
 import com.sweat.profile.util.setupCamera
 import com.sweat.profile.viewModel.AddFriendWithQRIntent
 import com.sweat.profile.viewModel.AddFriendWithQRScreenSideEffect
-import com.sweat.profile.viewModel.AddFriendWithQRScreenState
 import com.sweat.profile.viewModel.AddFriendWithQRViewModel
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun AddFriendWithQRRoute(
     modifier: Modifier = Modifier,
@@ -71,99 +68,77 @@ fun AddFriendWithQRRoute(
 
     val state by viewModel.state.collectAsStateWithLifecycle()
 
-    AddFriendWithQRScreen(
-        modifier = modifier,
-        state = state,
-        popupBackStack = popupBackStack,
-        handleIntent = viewModel::handleIntent,
-    )
-}
-
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun AddFriendWithQRScreen(
-    modifier: Modifier = Modifier,
-    state: AddFriendWithQRScreenState,
-    popupBackStack: () -> Unit,
-    handleIntent: (AddFriendWithQRIntent) -> Unit,
-) {
-    val context = LocalContext.current
-    val lifecycleOwner = context as? LifecycleOwner
-        ?: throw IllegalStateException("Context is not a LifecycleOwner")
-
-    // 카메라 권한 상태 관리
-    val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
-
-    // 권한 요청 처리
-    LaunchedEffect("cameraPermission") {
-        if (!cameraPermissionState.status.isGranted && !cameraPermissionState.status.shouldShowRationale) {
-            cameraPermissionState.launchPermissionRequest()
-        }
-    }
-
     if (state.isFinishAddFriend) {
-        Column(modifier = modifier.fillMaxSize()) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp, vertical = 16.dp),
-                horizontalArrangement = Arrangement.Start,
-            ) {
-                ChevronLeftIcon(modifier = Modifier.clickableSingle(onClick = popupBackStack))
-            }
-            Spacer(modifier = Modifier.height(34.dp))
-            AddFriendSuccessCard(friendName = state.friendName, context = context)
-        }
+        AddFriendSuccessScreen(
+            context = context,
+            friendName = state.friendName,
+            popupBackStack = popupBackStack
+        )
     } else if (
         cameraPermissionState.status.isGranted
         && !state.isFinishAddFriend
     ) {
-        // 권한이 허용된 경우에만 AndroidView 실행
-        Box(
-            modifier = modifier.fillMaxSize()
-        ) {
-            // QR 스캐너 뷰
-            AndroidView(
-                modifier = Modifier.fillMaxSize(),
-                factory = { ctx ->
-                    PreviewView(ctx).apply {
-                        post {
-                            setupCamera(
-                                previewView = this,
-                                lifecycleOwner = lifecycleOwner,
-                                onQrcodeScanned = {
-                                    handleIntent(
-                                        AddFriendWithQRIntent.AddFriend(
-                                            it ?: ""
-                                        )
+        AddFriendWithQRScreen(
+            modifier = modifier,
+            lifecycleOwner = lifecycleOwner,
+            popupBackStack = popupBackStack,
+            handleIntent = viewModel::handleIntent,
+        )
+    }
+}
+
+@Composable
+fun AddFriendWithQRScreen(
+    modifier: Modifier = Modifier,
+    lifecycleOwner: LifecycleOwner,
+    popupBackStack: () -> Unit,
+    handleIntent: (AddFriendWithQRIntent) -> Unit,
+) {
+    // 권한이 허용된 경우에만 AndroidView 실행
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        // QR 스캐너 뷰
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { ctx ->
+                PreviewView(ctx).apply {
+                    post {
+                        setupCamera(
+                            previewView = this,
+                            lifecycleOwner = lifecycleOwner,
+                            onQrcodeScanned = {
+                                handleIntent(
+                                    AddFriendWithQRIntent.AddFriend(
+                                        it ?: ""
                                     )
-                                },
-                            )
-                        }
+                                )
+                            },
+                        )
                     }
                 }
-            )
-
-            // Compose로 Top Bar
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp, vertical = 16.dp)
-                    .align(Alignment.TopStart),
-                horizontalArrangement = Arrangement.Start,
-            ) {
-                ChevronLeftIcon(modifier = Modifier.clickableSingle(onClick = popupBackStack))
             }
+        )
 
-            // QR 가이드 이미지
-            Icon(
-                painter = painterResource(R.drawable.qr_guide),
-                contentDescription = "QR Guide",
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .zIndex(1f)
-            )
+        // Compose로 Top Bar
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp, vertical = 16.dp)
+                .align(Alignment.TopStart),
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            ChevronLeftIcon(modifier = Modifier.clickableSingle(onClick = popupBackStack))
         }
+
+        // QR 가이드 이미지
+        Icon(
+            painter = painterResource(R.drawable.qr_guide),
+            contentDescription = "QR Guide",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .zIndex(1f)
+        )
     }
 }
 
@@ -173,6 +148,6 @@ fun AddFriendWithQRScreenPreview() {
     AddFriendWithQRScreen(
         handleIntent = { _ -> },
         popupBackStack = { },
-        state = AddFriendWithQRScreenState.getInitialState()
+        lifecycleOwner = LocalContext.current as LifecycleOwner
     )
 }
