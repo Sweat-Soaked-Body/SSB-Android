55086312256398ec37957cd4d0a0422a6ff93a4e
손찬형 <sonchanhyeong710@gmail.com>
2024-12-23 09:52:49 +0900

    pull 오류 수정

--- core/network/src/main/java/com/sweat/network/mapper/auth/LoginRequestMapper.kt
+++ core/network/src/main/java/com/sweat/network/mapper/auth/LoginRequestMapper.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:core/network/src/main/java/com/sweat/network/mapper/auth/request/LoginRequestMapper.kt
-package com.sweat.network.mapper.auth.request
-========
 package com.sweat.network.mapper.auth
->>>>>>>> 2d3943cd7146e56251ba2614453fdf04c8fa67a8:core/network/src/main/java/com/sweat/network/mapper/auth/LoginRequestMapper.kt
 
 import com.sweat.model.param.auth.LoginRequestParam
 import com.sweat.network.dto.auth.request.LoginRequest
