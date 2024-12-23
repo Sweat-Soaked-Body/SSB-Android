266301138c325a9d18e897e9bc6eea5ebc80ac91
손찬형 <sonchanhyeong710@gmail.com>
2024-12-23 14:15:16 +0900

    ExerciseNavigation에 AddExerciseNavigation 합치기

--- feature/exercise/src/main/java/com/sweat/exercise/ExerciseNavigation.kt
+++ feature/exercise/src/main/java/com/sweat/exercise/ExerciseNavigation.kt
@@ -4,12 +4,18 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.sweat.exercise.view.AddExerciseRoute
 import com.sweat.exercise.view.ExerciseRoute
 
 const val exerciseRoute = "exerciseRoute"
+const val addExerciseRoute = "addExerciseRoute"
 
 fun NavController.navigateToExerciseRoute(navOptions: NavOptions? = null){
     this.navigate(exerciseRoute, navOptions)
+}
+
+fun NavController.navigateToAddExerciseRoute(navOptions: NavOptions? = null){
+    this.navigate(addExerciseRoute, navOptions)
 }
 
 fun NavGraphBuilder.exerciseRoute(
@@ -18,6 +24,18 @@ fun NavGraphBuilder.exerciseRoute(
     composable(exerciseRoute){
         ExerciseRoute(
             navigateToAddExercise = navigateToAddExercise,
+        )
+    }
+}
+
+fun NavGraphBuilder.addExerciseRoute(
+    navigateToExercise: () -> Unit,
+    popUpBackStack: () -> Unit,
+){
+    composable(exerciseRoute){
+        AddExerciseRoute(
+            navigateToExercise = navigateToExercise,
+            popUpBackStack = popUpBackStack,
         )
     }
 }
