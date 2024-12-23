cc72b57b085f569da6630aa66372273659aeae2f
손찬형 <sonchanhyeong710@gmail.com>
2024-12-23 14:06:41 +0900

    ChevronLeftIcon의 clickableSingle 수정

--- feature/exercise/src/main/java/com/sweat/exercise/view/AddExerciseScreen.kt
+++ feature/exercise/src/main/java/com/sweat/exercise/view/AddExerciseScreen.kt
@@ -93,7 +93,7 @@ fun AddExerciseScreen(
                     modifier = modifier
                         .padding(1.dp)
                         .size(24.dp)
-                        .clickableSingle(onClick = { handleIntent(AddExerciseIntent.PopUpBackStack) })
+                        .clickableSingle{ handleIntent(AddExerciseIntent.PopUpBackStack) }
                 )
                 Text(
                     text = "운동 추가",
