08511093f50bb345111661691d9cb1f0db6475c7
youloveli <mymhj122@gmail.com>
2024-12-24 09:12:07 +0900

    feat: TopLevelDestination 이 각각의 목적지가 navHost에 등록되어있는 routeName을 가지도록 변경

--- app/src/main/java/com/sweat/ssb_android/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/sweat/ssb_android/navigation/TopLevelDestination.kt
@@ -4,20 +4,24 @@ import com.sweat.design_system.R
 
 enum class TopLevelDestination(
     val unselectedIcon: Int,
-    val iconText: String
+    val iconText: String,
+    val routeName: String,
 ) {
     Exercise(
         unselectedIcon = R.drawable.dumbbell,
-        iconText = "운동"
+        iconText = "운동",
+        routeName = "ExerciseRoute"
     ),
 
     Home(
         unselectedIcon = R.drawable.home,
-        iconText = "홈"
+        iconText = "홈",
+        routeName = "HomeRoute"
     ),
 
     Profile(
         unselectedIcon = R.drawable.profile_circle,
-        iconText = "프로필"
+        iconText = "프로필",
+        routeName = "ProfileRoute",
     )
 }
