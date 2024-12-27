ada3b378a973a8af3fdd999170360ad4f97021e9
명훈 <s23065@gsm.hs.kr>
2024-12-27 15:22:18 +0900

    :recycle: :: Refactor MainTimer Design

--- feature/main/src/main/java/com/school_of_company/main/view/component/MainTimer.kt
+++ feature/main/src/main/java/com/school_of_company/main/view/component/MainTimer.kt
@@ -3,17 +3,24 @@ package com.school_of_company.main.view.component
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -26,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.school_of_company.main.enum.TimerState
@@ -36,6 +44,7 @@ import com.sweat.design_system.icon.PlayIcon
 import com.sweat.design_system.icon.SkipIcon
 import com.sweat.design_system.icon.StopIcon
 import com.sweat.design_system.theme.SSBAndroidTheme
+import com.sweat.design_system.theme.SSBTypography
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -46,7 +55,8 @@ import kotlin.math.atan2
 internal fun MainTimer(
     modifier: Modifier = Modifier,
     initialTime: Int,
-    index: Int
+    index: Int,
+    nextOnClick: () -> Unit
 ) {
     SSBAndroidTheme { colors, typography ->
         val coroutineScope = rememberCoroutineScope()
@@ -111,12 +121,18 @@ internal fun MainTimer(
 
                             val dragDistance = dragAmount.getDistance()
 
-                            val newTimerValue = timerValue + (dragDistance * if (dragAngle > 0) 1 else -1).toInt()
+                            val newTimerValue =
+                                timerValue + (dragDistance * if (dragAngle > 0) 1 else -1).toInt()
 
                             timerValue = newTimerValue.coerceIn(0, initialTime)
 
                             coroutineScope.launch {
-                                progress.snapTo((timerValue.toFloat() / initialTime).coerceIn(0f, 1f))
+                                progress.snapTo(
+                                    (timerValue.toFloat() / initialTime).coerceIn(
+                                        0f,
+                                        1f
+                                    )
+                                )
                             }
                         }
                     }
@@ -188,7 +204,7 @@ internal fun MainTimer(
 
                 SkipIcon(
                     tint = colors.gray300,
-                    // todo : skip <- :main:tiemr 페이지 퍼블리싱할때 적용 / 지금 할 수 있는 부분이 아닌것 같다.. 하는 생각!
+                    modifier = Modifier.clickableSingle { nextOnClick() }
                 )
             }
         }
@@ -209,7 +225,8 @@ private fun MainTimerPreview() {
         MainTimer(
             modifier = Modifier,
             initialTime = 100,
-            index = 1
+            index = 1,
+            nextOnClick = {}
         )
     }
 }
