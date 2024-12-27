947a445dbfde14fa9be73f378771865a3a2d7cb6
손찬형 <sonchanhyeong710@gmail.com>
2024-12-27 15:37:21 +0900

    create ExerciseListResponse

--- core/network/src/main/java/com/sweat/network/dto/exercise/ExerciseListResponse.kt
+++ core/network/src/main/java/com/sweat/network/dto/exercise/ExerciseListResponse.kt
@@ -1,4 +1,13 @@
 package com.sweat.network.dto.exercise
 
-class ExerciseListResponse {
-}
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ExerciseListResponse (
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "category") val category: Int,
+    @Json(name = "service_user") val service_user: Int,
+    @Json(name = "like") val like: Boolean
+)
