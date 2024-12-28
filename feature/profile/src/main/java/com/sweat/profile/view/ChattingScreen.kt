773b7d9bc8765dea7db57ea0d78762cdf6b48914
youloveli <mymhj122@gmail.com>
2024-12-28 23:47:57 +0900

    feat: InitializeWebSocket 추가

--- feature/profile/src/main/java/com/sweat/profile/view/ChattingScreen.kt
+++ feature/profile/src/main/java/com/sweat/profile/view/ChattingScreen.kt
@@ -3,17 +3,10 @@ package com.sweat.profile.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -29,6 +22,10 @@ fun ChattingRoute(
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
+    LaunchedEffect(Unit) {
+        viewModel.handleIntent(ChattingIntent.InitializeWebSocket("1"))  // 웹소켓 URL 초기화
+    }
+
     ChattingScreen(
         modifier = modifier,
         state = state,
@@ -42,35 +39,12 @@ fun ChattingScreen(
     state: ChattingState,
     handleIntent: (ChattingIntent) -> Unit,
 ) {
-    var message by remember { mutableStateOf("") }  // 보내는 메시지
-
-    LaunchedEffect(Unit) {
-        handleIntent(ChattingIntent.InitializeWebSocket)  // 웹소켓 URL 초기화
-    }
-
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        // 받은 메시지 표시
-        Text(text = "Received Message: ${state.receivedMessage}", color = Color.Black)
 
-        // 메시지 보내기 텍스트 필드
-        BasicTextField(
-            value = message,
-            onValueChange = { message = it },
-            modifier = Modifier.padding(vertical = 16.dp),
-            singleLine = true
-        )
-
-        // 메시지 보내기 버튼
-        Button(onClick = {
-            handleIntent(ChattingIntent.SendMessage(message))  // 메시지 전송
-            message = ""  // 메시지 전송 후 입력창 비우기
-        }) {
-            Text("Send Message")
-        }
     }
 }
 
