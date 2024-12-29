0f612ebe66b6617072887b30eedbba26ab4e6b1f
youloveli <mymhj122@gmail.com>
2024-12-29 22:21:00 +0900

    feat: SettingsBottomSheet 에 onClick 추가

--- feature/profile/src/main/java/com/sweat/profile/component/bottomSheet/SettingsBottomSheet.kt
+++ feature/profile/src/main/java/com/sweat/profile/component/bottomSheet/SettingsBottomSheet.kt
@@ -4,26 +4,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.sweat.design_system.component.bottomSheet.BottomSheetItem
 import com.sweat.design_system.component.bottomSheet.SSBBottomSheet
+import com.sweat.design_system.component.modifier.clickableSingle
 import com.sweat.design_system.icon.OutIcon
 import com.sweat.design_system.icon.TrashIcon
 import com.sweat.design_system.theme.color.SSBColor
 
 @Composable
-fun SettingsBottomSheet(modifier: Modifier = Modifier) {
+fun SettingsBottomSheet(
+    modifier: Modifier = Modifier,
+    onClickMyQr: () -> Unit,
+    onClickMyAddFriendWithQr: () -> Unit,
+) {
     SSBBottomSheet(modifier = modifier) {
         BottomSheetItem(
+            modifier = Modifier.clickableSingle { onClickMyQr() },
             icon = { OutIcon() }, // TODO: 아이콘 변경
             title = "내 QR코드 보기",
             textColor = SSBColor.gray600
         )
         BottomSheetItem(
+            modifier = Modifier.clickableSingle { onClickMyAddFriendWithQr() },
             icon = { OutIcon() },// TODO: 아이콘 변경
             title = "QR 코드로 친구 추가",
-            textColor = SSBColor.gray600
-        )
-        BottomSheetItem(
-            icon = { TrashIcon() },// TODO: 아이콘 변경
-            title = "NFC로 친구 추가",
             textColor = SSBColor.gray600
         )
     }
