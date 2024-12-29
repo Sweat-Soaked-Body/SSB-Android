b4676022a4c57251cafd78f700393cf92e7c6aa0
youloveli <mymhj122@gmail.com>
2024-12-29 22:41:23 +0900

    feat: SettingsBottomSheet와 AddFriendBottomSheet의 내용을 서로 교체

--- feature/profile/src/main/java/com/sweat/profile/component/bottomSheet/AddFriendBottomSheet.kt
+++ feature/profile/src/main/java/com/sweat/profile/component/bottomSheet/AddFriendBottomSheet.kt
@@ -6,35 +6,26 @@ import com.sweat.design_system.component.bottomSheet.BottomSheetItem
 import com.sweat.design_system.component.bottomSheet.SSBBottomSheet
 import com.sweat.design_system.component.modifier.clickableSingle
 import com.sweat.design_system.icon.OutIcon
-import com.sweat.design_system.icon.PencilIcon
-import com.sweat.design_system.icon.TrashIcon
 import com.sweat.design_system.theme.color.SSBColor
 
 @Composable
 fun AddFriendBottomSheet(
     modifier: Modifier = Modifier,
-    onProfileEditClick: () -> Unit,
-    onLogoutClick: () -> Unit,
-    onLeaveClick: () -> Unit,
+    onClickMyQr: () -> Unit,
+    onClickMyAddFriendWithQr: () -> Unit,
 ) {
     SSBBottomSheet(modifier = modifier) {
         BottomSheetItem(
-            modifier = Modifier.clickableSingle { onProfileEditClick() },
-            icon = { PencilIcon() },
-            title = "프로필 수정",
+            modifier = Modifier.clickableSingle { onClickMyQr() },
+            icon = { OutIcon() }, // TODO: 아이콘 변경
+            title = "내 QR코드 보기",
             textColor = SSBColor.gray600
         )
         BottomSheetItem(
-            modifier = Modifier.clickableSingle { onLogoutClick() },
-            icon = { OutIcon() },
-            title = "로그 아웃",
+            modifier = Modifier.clickableSingle { onClickMyAddFriendWithQr() },
+            icon = { OutIcon() },// TODO: 아이콘 변경
+            title = "QR 코드로 친구 추가",
             textColor = SSBColor.gray600
-        )
-        BottomSheetItem(
-            modifier = Modifier.clickableSingle { onLeaveClick() },
-            icon = { TrashIcon() },
-            title = "회원 탈퇴",
-            textColor = SSBColor.error
         )
     }
 }
--- feature/profile/src/main/java/com/sweat/profile/component/bottomSheet/SettingsBottomSheet.kt
+++ feature/profile/src/main/java/com/sweat/profile/component/bottomSheet/SettingsBottomSheet.kt
@@ -6,27 +6,35 @@ import com.sweat.design_system.component.bottomSheet.BottomSheetItem
 import com.sweat.design_system.component.bottomSheet.SSBBottomSheet
 import com.sweat.design_system.component.modifier.clickableSingle
 import com.sweat.design_system.icon.OutIcon
+import com.sweat.design_system.icon.PencilIcon
 import com.sweat.design_system.icon.TrashIcon
 import com.sweat.design_system.theme.color.SSBColor
 
 @Composable
 fun SettingsBottomSheet(
     modifier: Modifier = Modifier,
-    onClickMyQr: () -> Unit,
-    onClickMyAddFriendWithQr: () -> Unit,
+    onProfileEditClick: () -> Unit,
+    onLogoutClick: () -> Unit,
+    onLeaveClick: () -> Unit,
 ) {
     SSBBottomSheet(modifier = modifier) {
         BottomSheetItem(
-            modifier = Modifier.clickableSingle { onClickMyQr() },
-            icon = { OutIcon() }, // TODO: 아이콘 변경
-            title = "내 QR코드 보기",
+            modifier = Modifier.clickableSingle { onProfileEditClick() },
+            icon = { PencilIcon() },
+            title = "프로필 수정",
             textColor = SSBColor.gray600
         )
         BottomSheetItem(
-            modifier = Modifier.clickableSingle { onClickMyAddFriendWithQr() },
-            icon = { OutIcon() },// TODO: 아이콘 변경
-            title = "QR 코드로 친구 추가",
+            modifier = Modifier.clickableSingle { onLogoutClick() },
+            icon = { OutIcon() },
+            title = "로그 아웃",
             textColor = SSBColor.gray600
+        )
+        BottomSheetItem(
+            modifier = Modifier.clickableSingle { onLeaveClick() },
+            icon = { TrashIcon() },
+            title = "회원 탈퇴",
+            textColor = SSBColor.error
         )
     }
 }
