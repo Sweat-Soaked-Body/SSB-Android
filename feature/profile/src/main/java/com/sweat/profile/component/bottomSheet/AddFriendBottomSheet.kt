e664458fd8644130959dd48551b0f05258e5ea88
youloveli <mymhj122@gmail.com>
2024-12-29 22:20:52 +0900

    feat: AddFriendBottomSheet 에 onClick 추가

--- feature/profile/src/main/java/com/sweat/profile/component/bottomSheet/AddFriendBottomSheet.kt
+++ feature/profile/src/main/java/com/sweat/profile/component/bottomSheet/AddFriendBottomSheet.kt
@@ -4,27 +4,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.sweat.design_system.component.bottomSheet.BottomSheetItem
 import com.sweat.design_system.component.bottomSheet.SSBBottomSheet
+import com.sweat.design_system.component.modifier.clickableSingle
 import com.sweat.design_system.icon.OutIcon
 import com.sweat.design_system.icon.PencilIcon
 import com.sweat.design_system.icon.TrashIcon
 import com.sweat.design_system.theme.color.SSBColor
 
 @Composable
-fun AddFriendBottomSheet(modifier: Modifier = Modifier) {
+fun AddFriendBottomSheet(
+    modifier: Modifier = Modifier,
+    onProfileEditClick: () -> Unit,
+    onLogoutClick: () -> Unit,
+    onLeaveClick: () -> Unit,
+) {
     SSBBottomSheet(modifier = modifier) {
         BottomSheetItem(
+            modifier = Modifier.clickableSingle { onProfileEditClick() },
             icon = { PencilIcon() },
             title = "프로필 수정",
             textColor = SSBColor.gray600
         )
         BottomSheetItem(
+            modifier = Modifier.clickableSingle { onLogoutClick() },
             icon = { OutIcon() },
             title = "로그 아웃",
             textColor = SSBColor.gray600
         )
         BottomSheetItem(
+            modifier = Modifier.clickableSingle { onLeaveClick() },
             icon = { TrashIcon() },
-            title = "프로필 삭제",
+            title = "회원 탈퇴",
             textColor = SSBColor.error
         )
     }
