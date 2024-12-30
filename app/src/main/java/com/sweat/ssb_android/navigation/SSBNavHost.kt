537c5fae7429b46005898fcd865f4d6d7ac76942
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2024-12-30 10:45:21 +0900

    navigation connection

--- app/src/main/java/com/sweat/ssb_android/navigation/SSBNavHost.kt
+++ app/src/main/java/com/sweat/ssb_android/navigation/SSBNavHost.kt
@@ -18,8 +18,12 @@ import com.sweat.exercise.addExerciseRoute
 import com.sweat.exercise.exerciseRoute
 import com.sweat.exercise.navigateToAddExercise
 import com.sweat.login.loginRoute
+import com.sweat.login.navigateToLoginRoute
 import com.sweat.profile.addFriendWithQRRoute
 import com.sweat.profile.friendQrGenerateRoute
+import com.sweat.profile.navigateToAddFriendWithQR
+import com.sweat.profile.navigateToFriendQrGenerate
+import com.sweat.profile.navigateToProfileRoute
 import com.sweat.profile.profileRoute
 import com.sweat.signup.navigation.navigateToSignupRoute
 import com.sweat.signup.navigation.signupRoute
