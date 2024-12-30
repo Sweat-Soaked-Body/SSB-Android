1df6629f0260e35331d7020c38090cd5d5ea75c9
youloveli <mymhj122@gmail.com>
2024-12-31 08:43:20 +0900

    feat: 기능 구현

--- feature/profile/src/main/java/com/sweat/profile/viewModel/ChattingViewModel.kt
+++ feature/profile/src/main/java/com/sweat/profile/viewModel/ChattingViewModel.kt
@@ -3,6 +3,7 @@ package com.sweat.profile.viewModel
 import android.util.Log
 import com.sweat.common.base.BaseViewModel
 import com.sweat.network.BuildConfig
+import com.sweat.network.dto.profile.ChatMessage
 import com.sweat.network.util.WebSocketClient
 import dagger.hilt.android.lifecycle.HiltViewModel
 import okhttp3.OkHttpClient
@@ -18,7 +19,7 @@ class ChattingViewModel @Inject constructor(
         client = okHttpClient,
         onMessageReceived = { message ->
             Log.d("WebSocketClient", "Message received: $message")
-            addMessageToList(message)
+            addListMessageToList(messages = message)
         },
         onError = { error ->
             Log.e("WebSocketClient", "Error: ${error.message}")
@@ -35,7 +36,7 @@ class ChattingViewModel @Inject constructor(
     override fun handleIntent(intent: ChattingIntent) {
         when (intent) {
             is ChattingIntent.SetMyName -> setState { copy(myName = intent.name) }
-            is ChattingIntent.SendMessage -> sendMessage(intent.message)
+            is ChattingIntent.SendMessage -> sendMessage()
             is ChattingIntent.InitializeWebSocket -> initializeWebSocket(intent.roomId)
             is ChattingIntent.SetMessageInputTextState -> setState { copy(messageInputTextState = intent.state) }
         }
@@ -46,17 +47,21 @@ class ChattingViewModel @Inject constructor(
         webSocketClient.connect(roomName = roomName)
     }
 
-    private fun sendMessage(message: String) {
-        webSocketClient.sendMessage(message)
+    private fun sendMessage() {
+        addMessageToList(state.value.messageInputTextState)
+        webSocketClient.sendMessage(state.value.messageInputTextState)
     }
 
     private fun addMessageToList(message: String) {
         val newMessage = ChatMessage(
             message = message,
-            sender = "Server", // 기본값으로 설정 (필요에 따라 파싱)
-            timestamp = System.currentTimeMillis() // 타임스탬프 추가
+            user = state.value.myName, // 기본값으로 설정 (필요에 따라 파싱)
+            timestamp = System.currentTimeMillis().toString() // 타임스탬프 추가
         )
         setState { copy(messages = messages + newMessage) }
+    }
+    private fun addListMessageToList(messages: List<ChatMessage>) {
+        setState { copy(messages = this.messages + messages) }
     }
 }
 
@@ -75,12 +80,6 @@ data class ChattingState(
         )
     }
 }
-
-data class ChatMessage(
-    val message: String,
-    val sender: String,
-    val timestamp: Long
-)
 
 sealed class ChattingScreenSideEffect
 
