b7171690ae8e8b77ea2efe9f6e0860e813164135
youloveli <mymhj122@gmail.com>
2024-12-31 08:44:58 +0900

    feat: mainRoute -> homeRoute 변경

--- app/src/main/java/com/sweat/ssb_android/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/sweat/ssb_android/navigation/TopLevelDestination.kt
@@ -1,6 +1,6 @@
 package com.sweat.ssb_android.navigation
 
-import com.school_of_company.main.navigation.mainRoute
+import com.school_of_company.main.navigation.homeRoute
 import com.sweat.design_system.R
 import com.sweat.exercise.exerciseRoute
 import com.sweat.profile.profileRoute
@@ -19,7 +19,7 @@ enum class TopLevelDestination(
     Home(
         unselectedIcon = R.drawable.home,
         iconText = "홈",
-        routeName = mainRoute
+        routeName = homeRoute
     ),
 
     Profile(
